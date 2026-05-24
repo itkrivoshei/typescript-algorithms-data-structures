448330242eff7e5ac247f9291f38ee46dbb802cc
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-24 15:27:51 +0200

    style: format files covered by prettier check

--- src/BinarySearchTrees/prettyPrint.ts
+++ src/BinarySearchTrees/prettyPrint.ts
@@ -1,17 +1,11 @@
 import { TreeNode } from './TreeNode';
 
-export const prettyPrint = <T>(
-  node: TreeNode<T> | null,
-  prefix = '',
-  isLeft = true
-) => {
+export const prettyPrint = <T>(node: TreeNode<T> | null, prefix = '', isLeft = true) => {
   if (node === null) return;
 
-  if (node.right !== null)
-    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  if (node.right !== null) prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
 
   console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
 
-  if (node.left !== null)
-    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  if (node.left !== null) prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
 };
